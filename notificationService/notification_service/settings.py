e025561021f94e1959134ee8ea2d61c665e2c1bb
AkshayBharathan <akshay.devstack@gmail.com>
2026-07-07 14:21:17 +0530

    Notification service celery-beat task name mistake is fixed

--- notificationService/notification_service/settings.py
+++ notificationService/notification_service/settings.py
@@ -216,7 +216,7 @@ CELERY_TASK_DEFAULT_QUEUE = "notification_queue"
 CELERY_BEAT_SCHEDULE = {
     "delete-readed-notifications-every-morning": {
         "task": "celery_beat.tasks.delete_readed_notifications",
-        "schedule": crontab(hour=11, minute=15),
+        "schedule": crontab(),
         "options": {"queue": "notification_queue"},
     }
 }
